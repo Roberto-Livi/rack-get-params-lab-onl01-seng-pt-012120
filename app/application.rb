9de0e86f03e26791af112e3fb53997149a60e450
Roberto Livi <robertomlivi@outlook.com>
2020-03-28 21:38:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,10 +16,10 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        resp.write "Cart is empty"
+        resp.write "Your cart is empty"
       else
         @@cart.each do |item|
-          resp.write item
+          resp.write "#{item}\n"
         end
       end
     elsif req.path.match(/add/)
